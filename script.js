85655b4acd126953ff5249c124d4c428994eb697
lecksa <dolabesipa@gmail.com>
2024-02-07 23:44:23 +0500

    added

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+import { reload } from "./function.js"
+
 let body = document.body
 // let form = document.forms.todolist
 let wrap = document.querySelector('.wrap')
@@ -38,34 +40,3 @@ submit.onclick = () => {
         todo.value = ''
     }
 }
-
-function reload(place, your_list) {
-    place.innerHTML = ""
-
-    for (let item of your_list) {
-
-        let box = document.createElement('div')
-        let text = document.createElement('div')
-        let h1 = document.createElement('h1')
-        let btn = document.createElement('button')
-        let p = document.createElement('p')
-
-        box.classList.add('box')
-        text.classList.add('text')
-
-        h1.innerHTML = item.value
-        btn.innerHTML = "x"
-        p.innerHTML = item.time
-
-        place.append(box)
-        box.append(text, p)
-        text.append(h1, btn)
-
-        btn.onclick = () => {
-            your_list.splice(your_list.indexOf(item), 1)
-            box.remove()
-        }
-    }
-}
-
-
